bc65a935249b7943297f9986ba9f4df7f4c47112
kimtuan1102 <kimtuan1102@gmail.com>
2020-08-21 13:47:41 +0700

    change app name

--- ios/eamakersTests/eamakersTests.m
+++ ios/eamakersTests/eamakersTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface BoilerplateTests : XCTestCase
+@interface eamakersTests : XCTestCase
 
 @end
 
-@implementation BoilerplateTests
+@implementation eamakersTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
